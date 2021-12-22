b2bfe2fe99102ad644dc250b4cd77fe1b4847c47
Серикжан Навий <serj_navii@mail.ru>
2021-12-22 22:46:29 +0600

    feat: add logger options

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,30 @@
 import Fastify from 'fastify';
 import config from './common/config';
+import path from 'path';
 
-const fastify = Fastify({ logger: true });
+const today = new Date().toJSON().slice(0, 10).split('-').reverse().join('-');
+
+const fastify = Fastify({
+  logger: {
+    // prettyPrint: true,
+    file: path.join(__dirname, `./logs/log-${today}.log`),
+    level: 'info',
+    serializers: {
+      res (reply) {
+        return {
+          statusCode: reply.statusCode,
+        }
+      },
+      req (request) {
+        return {
+          method: request.method,
+          url: request.url,
+          parameters: request.params,
+        }
+      }
+    }
+  },
+});
 
 fastify.register(require('fastify-swagger'), {
   exposeRoute: true,
